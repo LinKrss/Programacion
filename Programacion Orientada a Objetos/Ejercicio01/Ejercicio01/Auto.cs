292597936d864a85faf7bbf1634b39d6827ffd47
Cristian Lin <cristianlin40@gmail.com>
2023-04-05 09:26:08 -0300

    Add files via upload

---
+++ Programacion Orientada a Objetos/Ejercicio01/Ejercicio01/Auto.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ejercicio01
+{
+    internal class Auto
+    {
+        private string _marca;
+        private string _modelo;
+        private int _velocidad;
+
+        public void Acelerar()
+        {
+            _velocidad += 5;
+        }
+
+        public void Acelerar(int velocidad)
+        {
+            _velocidad += _velocidad;
+        }
+
+        public void Frenar()
+        {
+            _velocidad -= 5;
+        }
+
+        public void Frenar(int velocidad)
+        {
+            _velocidad -= _velocidad;
+        }
+    }
+}
---
+++ Programacion Orientada a Objetos/Ejercicio01/Ejercicio01/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ejercicio01
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Auto auto = new Auto();
+            
+        auto.Acelerar();
+            
+        }
+    }
+}
