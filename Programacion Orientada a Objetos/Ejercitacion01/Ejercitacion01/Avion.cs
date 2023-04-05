36b32f9c254883f81aeddefaf329ac2594a10847
Cristian Lin <cristianlin40@gmail.com>
2023-04-05 12:49:35 -0300

    Add files via upload

---
+++ Programacion Orientada a Objetos/Ejercitacion01/Ejercitacion01/Avion.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ejercitacion01
+{
+    internal class Avion
+    {
+        private string _compañia;
+        private string _modelo;
+        private int _velocidad;
+        private int _altura;
+
+        public void Acelerar()
+        {
+            _velocidad += 5;
+        }
+
+        public void Acelerar(int velocidad)
+        {
+            _velocidad += velocidad;
+        }
+
+        public void Desacelerar()
+        {
+            _velocidad -= 5;
+        }
+
+        public void Desacelerar(int velocidad)
+        {
+            _velocidad -= velocidad;
+        }
+
+        public void Subir()
+        {
+            _altura += 1;
+        }
+
+        public void Subir(int altura)
+        {
+            _altura += altura;
+        }
+
+        public void Bajar()
+        {
+            _altura -= 1;
+        }
+
+        public void Bajar(int altura)
+        {
+            _altura -= altura;
+        }
+    }
+   
+}
