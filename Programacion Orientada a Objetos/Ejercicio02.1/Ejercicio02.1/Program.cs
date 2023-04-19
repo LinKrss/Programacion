fd476bc18f65f3c642c50815a0359549104ac8af
Cristian Lin <cristianlin40@gmail.com>
2023-04-19 00:09:51 -0300

    Add files via upload

--- Programacion Orientada a Objetos/Ejercicio02.1/Ejercicio02.1/Program.cs
+++ Programacion Orientada a Objetos/Ejercicio02.1/Ejercicio02.1/Program.cs
@@ -10,21 +10,21 @@ namespace Ejercicio02._1
     {
         static void Main(string[] args)
         {
-            Titular titular = new Titular();
+            Titular miTitular = new Titular();
 
-            Console.WriteLine("Ingrese el nombre del titular de la cuenta");
-            titular.Nombre = Console.ReadLine();
+            Console.WriteLine("Ingrese el nombre del titular: ");
+            miTitular.Nombre = Console.ReadLine();
 
-            Console.WriteLine("Ingrese el apellido del titular de la cuenta");
-            titular.Apellido = Console.ReadLine();
+            Console.WriteLine("Ingrese el apellido del titular: ");
+            miTitular.Apellido = Console.ReadLine();
 
-            Console.WriteLine("Ingrese el Dni del titular de la cuenta");
-            titular.Dni = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Ingrese el DNI del titular: ");
+            miTitular.Dni = Convert.ToInt32(Console.ReadLine());
 
-            Console.WriteLine("Ingrese la fecha de nacimiento del titular de la cuenta (dd/mm/aaaa)");
-            titular.FechaNacimiento = Convert.ToDateTime(Console.ReadLine());
+            Console.WriteLine("Ingrese la fecha de nacimiento del titular de la cuenta (dd/mm/aaaa): ");
+            miTitular.FechaNacimiento = Convert.ToDateTime(Console.ReadLine());
 
-            Console.WriteLine("La edad es: " + titular.Edad);
+            Console.WriteLine("La edad es: " + miTitular.Edad);
 
             Console.ReadKey();
 
