e1c2fa999df761565738cf5e2d57200223f3b8e8
Cristian Lin <cristianlin40@gmail.com>
2023-04-27 23:33:29 -0300

    Add files via upload

---
+++ Programacion Orientada a Objetos/Ejercicio 3/Ejercicio 3/Alumno.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ejercicio_3
+{
+    internal class Alumno
+    {
+        private int _Legajo;
+        private string _Nombre;
+        private string _Apellido;
+        private DateTime _Fecha_Nacimiento;
+        private DateTime _Fecha_Ingreso;
+        private bool _Activo;
+        private int _Cant_Materia_Aprobadas;
+
+
+    }
+}
---
+++ Programacion Orientada a Objetos/Ejercicio 3/Ejercicio 3/ListaAlumnos.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ejercicio_3
+{
+    internal class ListaAlumnos
+    {
+        private List<Alumno> alumnos;                   //Atributo
+
+        public ListaAlumnos()                           //Constructor
+        {
+            alumnos = new List<Alumno>();               //Instanciar alumnos
+        }
+
+        public void AgregarAlumno(Alumno alumno)
+        {
+            alumnos.Add(alumno);
+        }
+    }
+}
