655c4e9458d2cf9091e97df35344e5848db1c1e4
Cristian Lin <cristianlin40@gmail.com>
2023-05-02 16:27:36 -0300

    Add files via upload

--- Programacion Orientada a Objetos/Ejercicio 3/Ejercicio 3/Alumno.cs
+++ Programacion Orientada a Objetos/Ejercicio 3/Ejercicio 3/Alumno.cs
@@ -8,7 +8,7 @@ namespace Ejercicio_3
 {
     internal class Alumno
     {
-        private int _Legajo;
+        private string _Legajo;
         private string _Nombre;
         private string _Apellido;
         private DateTime _Fecha_Nacimiento;
@@ -16,6 +16,44 @@ namespace Ejercicio_3
         private bool _Activo;
         private int _Cant_Materia_Aprobadas;
 
+        public string NumeroLegajo
+        {
+            get { return _Legajo; }
+            set { _Legajo = value; }
+        }
 
+        public string Nombre
+        { 
+            get { return _Nombre; }
+            set { _Nombre = value; }
+        }
+        
+        public string Apellido
+        { 
+            get { return _Apellido; }
+            set { _Apellido  = value; }        
+        }
+
+        public DateTime FechaNacimiento
+        {
+            get { return _Fecha_Nacimiento; }
+            set { _Fecha_Nacimiento = value; }
+        }
+
+        public DateTime FechaIngreso
+        {
+            get { return _Fecha_Ingreso; }
+        }
+
+        public bool Activo
+        {
+            get { return _Activo; }
+            set { _Activo = value; }
+        }
+
+        public int CantidadMateriasAprobadas
+        {
+            get { return _Cant_Materia_Aprobadas; }
+        }
     }
 }
--- Programacion Orientada a Objetos/Ejercicio 3/Ejercicio 3/ListaAlumnos.cs
+++ Programacion Orientada a Objetos/Ejercicio 3/Ejercicio 3/ListaAlumnos.cs
@@ -8,16 +8,55 @@ namespace Ejercicio_3
 {
     internal class ListaAlumnos
     {
-        private List<Alumno> alumnos;                   //Atributo
+        private List<Alumno> listaAlumnos;                   //Atributo
 
         public ListaAlumnos()                           //Constructor
         {
-            alumnos = new List<Alumno>();               //Instanciar alumnos
+            listaAlumnos = new List<Alumno>();               //Instanciar alumnos
         }
 
-        public void AgregarAlumno(Alumno alumno)
+        public string AgregarAlumno(Alumno alumno)
         {
-            alumnos.Add(alumno);
+            var alumnoExiste = listaAlumnos.FirstOrDefault(alumnoExistente => alumnoExistente.NumeroLegajo == alumno.NumeroLegajo);
+            if (alumnoExiste != null)
+            {
+                return "El numero de legajo " + alumno.NumeroLegajo + " ya existe.";
+            }
+            else
+            {
+                listaAlumnos.Add(alumno);
+                return "El alumno " + alumno.Nombre + " " + alumno.Apellido + " ha sido registrado correctamente";
+            }
+        }
+        public IReadOnlyCollection<Alumno> ListarAlumnos()
+        {
+            return listaAlumnos.AsReadOnly();
+        }
+        public string RemoverAlumno(Alumno alumno)
+        {
+            var alumnoRemovido = listaAlumnos.Remove(alumno);
+            if (alumnoRemovido)
+            {
+                return "El alumno: " + alumno.Nombre + " " + alumno.Apellido + " Fue removido de la lista de alumnos.";
+            }
+            else
+            {
+                return "El alumno ingresado no pertenece al listado de alumnos";
+            }
+        }
+        public string RemoverAlumno(string numeroLegajo)
+        {
+            var alumnoExiste = listaAlumnos.FirstOrDefault(alumnoExistente => alumnoExistente.NumeroLegajo == numeroLegajo);
+            if (alumnoExiste != null)
+            {
+                listaAlumnos.Remove(alumnoExiste);
+                return "El alumno " + alumnoExiste.Nombre + " " + alumnoExiste.Apellido + " ha sido removido correctamente";
+            }
+            else
+            {
+                return "Numero de legajo inexistente";
+            }
         }
     }
 }
+
